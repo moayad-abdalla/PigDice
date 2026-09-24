c7490cad257de50d22f8581b5081c61945d3497f
almoayadabdalla <moayad@umich.edu>
2026-09-24 14:01:13 -0400

    Dei class added

--- PigDice.cpp
+++ PigDice.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <random>
 
-// Build your solution starting from this code.
+
 
 struct GameState {
     char choice;
@@ -13,96 +14,146 @@ struct GameState {
     bool turn_over = false;
 };
 
-void play_game(GameState &g);
-void take_turn(GameState &g);
-void roll(GameState &g);
-void hold(GameState &g);
-void display_rules();
 
-int main() {
-    GameState my_game;
 
-    display_rules();
+class Die {
+private:
+    int m_value;
+    int m_numOfSides;
 
-    play_game(my_game);
+public:
+    int m_test;
 
-    return 0;
-}
+    Die() { //deafult constructor
+        m_numOfSides = 6;
+        setValue();
+    }
 
-void display_rules() {
-    std::cout << "Let's Play PIG Dice!" << std::endl;
-}
-
-void play_game(GameState &g) {
-    while (!g.game_over) {
-        take_turn(g);
-
-        g.game_score += g.score_this_turn;
-
-        if (g.game_score >= 20) {
-            g.game_over = true;
-        }
-        else {
-            g.turn_over = false;
-            g.score_this_turn = 0;
+    // Allows only a 4, 6, or 8 sided die
+    void setNumOfSides(int numOfSides) {
+        switch (numOfSides) {
+            case 4:
+                m_numOfSides = 4;
+                break;
+            case 6:
+                m_numOfSides = 6;
+                break;
+            case 8:
+                m_numOfSides = 8;
+                break;
+            default:
+                m_numOfSides = 6;
         }
     }
 
-    std::cout << "You finished with a final score of ";
-    std::cout << g.game_score;
-    std::cout << " in " << g.turn_count << " turns! ";
-    std::cout << "\nThanks for playing PIG Dice!";
+    void setValue() {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distr(1, m_numOfSides);
+        m_value = distr(gen);
+    }
+
+    // Returns die value
+    int getValue() {
+        return m_value;
+    }
+
+    // Returns the number of sides
+    int getNumOfSides() {
+        return m_numOfSides;
+    }
+
+    //Rolls the die and updates the current turn
+    void roll (GameState &g) {
+        /*srand(time(NULL))
+         *int die = rand () %6 +1%; */
+        Die myDie; // calls the default constructor
+        //myDie.setValue(); // calling the public function to roll the die
+        std::cout <<"Die: " << myDie.getValue();
+        if (myDie.getValue() == 1) {
+            std::cout << "\nTurn over. No score. \n";
+            g.score_this_turn = 0;
+            g.turn_over = true;
+        }
+        else {
+            g.score_this_turn+=myDie.getValue();
+            std::cout << " - Running score this turn: " << g.score_this_turn;
+        }
+        std::cout << std::endl;
+    }
+};
+
+
+
+// Rules Display
+void displayRules() {
+    std::cout << "Let's Play PIG Dice!\n";
+    std::cout << "* See how many turns it takes you to get to 20 points.\n";
+    std::cout << "* Turn ends when you hold or roll a 1.\n";
+    std::cout << "* If you roll a 1, you lose all points for the turn.\n";
+    std::cout << "* If you hold, you bank all points for the turn to the game score.\n";
 }
 
-void take_turn(GameState &g) {
-    g.turn_count++;
+//Adds turn score to the total game score
+void hold(GameState &g) {
+    g.game_score += g.score_this_turn;
+    g.turn_over = true;
 
-    std::cout << "\nTURN " << g.turn_count;
-    std::cout << " - Game Score: " << g.game_score << std::endl;
+    if (g.game_score >= 20) {
+        g.game_over = true;
+    }
+}
 
-    while (!g.turn_over) {
+// Controls one turn
+void takeTurn(GameState &g, Die &myDie) {
+    g.turn_over = false;
+    g.score_this_turn = 0;
+
+    std::cout << "\nTURN " << g.turn_count + 1
+              << " - Game Score: " << g.game_score << std::endl;
+
+    while (g.turn_over == false) {
         std::cout << "roll or hold? (r/h): ";
         std::cin >> g.choice;
 
-        if (g.choice == 'r') {
-            roll(g);
+        if (g.choice == 'r' || g.choice == 'R') {
+            myDie.roll(g);
         }
-        else if (g.choice == 'h') {
+        else if (g.choice == 'h' || g.choice == 'H') {
             hold(g);
         }
         else {
-            std::cout << "Invalid Choice!" << std::endl;
+            // Invalid input to give player another chance
+            std::cout << "Invalid input. Please enter r or h.\n";
         }
     }
 
     std::cout << "Score Banked This Turn: "
               << g.score_this_turn << std::endl;
+
+    //completed turn increases the turn count
+    g.turn_count++;
+    g.score_this_turn = 0;
 }
 
-void roll(GameState &g) {
-    srand(time(NULL));
-
-    int die = rand() % 6 + 1;
-
-    std::cout << "Die: " << die;
-
-    if (die == 1) {
-        std::cout << std::endl;
-        std::cout << "Turn over. No Score." << std::endl;
-
-        g.score_this_turn = 0;
-        g.turn_over = true;
+//Keeps playing until the score reaches at least 20
+void playGame(GameState &g, Die &myDie) {
+    while (g.game_over == false) {
+        takeTurn(g, myDie);
     }
-    else {
-        g.score_this_turn += die;
 
-        std::cout << " - Running score this turn: "
-                  << g.score_this_turn << std::endl;
-    }
+    std::cout << "\nYou finished with a final score of "
+              << g.game_score << " in "
+              << g.turn_count << " turns!\n";
+    std::cout << "Thanks for playing PIG Dice!\n";
 }
 
-void hold(GameState &g) {
-    g.turn_over = true;
+int main() {
+    GameState my_game; // instantiate a GameState object
+    Die myDie; // calls the default constructor
 
+    displayRules();
+    playGame(my_game, myDie);
 
+    return 0;
 }
